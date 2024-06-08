3f25002b158d4222cd7a6c5bd84aa65fc1ca46ef
q67aaa <q67aaa@gmail.com>
2024-06-08 21:25:14 +0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,9 +6,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_TECNO-LE7.mk
+    $(LOCAL_DIR)/twrp_TECNO-LE7n.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_TECNO-LE7-user \
-    twrp_TECNO-LE7-userdebug \
-    twrp_TECNO-LE7-eng
+    twrp_TECNO-LE7n-user \
+    twrp_TECNO-LE7n-userdebug \
+    twrp_TECNO-LE7n-eng
