ddf8e4e2dd971f72dddbbdfee58370c8587deaec
q67aaa <q67aaa@gmail.com>
2024-06-09 10:28:24 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,7 +18,7 @@
 
 # Allow building with minimal manifest
 
-DEVICE_PATH := device/TECNO/TECNO_LE7n
+DEVICE_PATH := device/TECNO/LE7n
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
